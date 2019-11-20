034a20c108f01142e0e32a93e446ee52df8d1c4e
klimas7 <klimas7@gmail.com>
2019-11-20 14:46:40 +0100

    annotation configuration #3: Hello world class

---
+++ core/src/main/java/pl/sii/spring/core/annotation/HelloWorld.java
@@ -0,0 +1,20 @@
+package pl.sii.spring.core.annotation;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class HelloWorld {
+    private String message;
+    private String message2;
+
+    void print() {
+        System.out.println("Your messages: " + message + " : " + message2);
+    }
+}
+
--- core/src/main/java/pl/sii/spring/core/annotation/MainClass.java
+++ core/src/main/java/pl/sii/spring/core/annotation/MainClass.java
@@ -7,8 +7,16 @@ import java.util.Arrays;
 
 public class MainClass {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext("pl.sii.spring.annotation");
+        ApplicationContext context = new AnnotationConfigApplicationContext("pl.sii.spring.core.annotation");
         System.out.println(context.getBeanDefinitionCount());
         Arrays.asList(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+        System.out.println("----------------");
+
+        HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
+        helloWorld.print();
+
+        HelloWorld helloWorldBySetter = (HelloWorld) context.getBean("helloWorldSetter");
+        helloWorldBySetter.print();
     }
 }
