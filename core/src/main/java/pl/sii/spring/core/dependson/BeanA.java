2d7e127ac9cec62c8cc20113a1f77eb4dca55906
klimas7 <klimas7@gmail.com>
2019-11-22 23:43:10 +0100

    Depends on #2

--- core/src/main/java/pl/sii/spring/core/dependson/BeanA.java
+++ core/src/main/java/pl/sii/spring/core/dependson/BeanA.java
@@ -1,8 +1,10 @@
 package pl.sii.spring.core.dependson;
 
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
 @Component
+@DependsOn("beanB")
 public class BeanA {
     public BeanA() {
         System.out.println("BeanA");
--- core/src/main/java/pl/sii/spring/core/dependson/BeanB.java
+++ core/src/main/java/pl/sii/spring/core/dependson/BeanB.java
@@ -1,8 +1,10 @@
 package pl.sii.spring.core.dependson;
 
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
 @Component
+@DependsOn("beanC")
 public class BeanB {
     public BeanB() {
         System.out.println("BeanB");
