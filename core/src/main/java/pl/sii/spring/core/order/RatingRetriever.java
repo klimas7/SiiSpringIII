d08ca67147a384c5e3939c9c9152f4535ac8aebd
klimas7 <klimas7@gmail.com>
2019-11-22 22:03:34 +0100

    Order example #2: Add rating

---
+++ core/src/main/java/pl/sii/spring/core/order/RatingRetriever.java
@@ -0,0 +1,17 @@
+package pl.sii.spring.core.order;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class RatingRetriever {
+    @Autowired
+    private Rating rating;
+
+    @PostConstruct
+    public void printRating() {
+        System.out.println(rating);
+    }
+}
