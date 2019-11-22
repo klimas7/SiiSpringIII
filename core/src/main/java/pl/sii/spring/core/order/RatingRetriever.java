4e14102f25d827ef3fa300f72d7dc1b0fc910c65
klimas7 <klimas7@gmail.com>
2019-11-22 22:39:18 +0100

    Order example #4: change as ratings

--- core/src/main/java/pl/sii/spring/core/order/RatingRetriever.java
+++ core/src/main/java/pl/sii/spring/core/order/RatingRetriever.java
@@ -5,15 +5,21 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Component
 public class RatingRetriever {
-    @Autowired
-    @Qualifier("excellent")
-    private Rating rating;
+    private final List<Rating> ratings; //As alphabet
+
+    public RatingRetriever(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
+    //DefaultListableBeanFactory.getBeanNamesForType --> this.allBeanNamesByType Tutaj jest zgodnie z alfabetem
+    //ClassPathBeanDefinitionScanner -> doScan
+
 
     @PostConstruct
     public void printRating() {
-        System.out.println("Rating " + rating.getRating());
+        ratings.forEach(rating -> System.out.println("Rating: " + rating.getRating()));
     }
 }
