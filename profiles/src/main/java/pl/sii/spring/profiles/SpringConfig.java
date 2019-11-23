645c80cee46ca0cf7755efd67528ada14e21e0dc
klimas7 <klimas7@gmail.com>
2019-11-23 15:59:39 +0100

    profiles #5: Try to add specyfic property files

--- profiles/src/main/java/pl/sii/spring/profiles/SpringConfig.java
+++ profiles/src/main/java/pl/sii/spring/profiles/SpringConfig.java
@@ -1,23 +1,29 @@
 package pl.sii.spring.profiles;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
 @Configuration
 @PropertySource("classpath:application.properties")
+@PropertySource("classpath:application-dev.properties")
+@PropertySource("classpath:application-test.properties")
 public class SpringConfig {
+
+    @Value("${extraMessage}")
+    private String extraMessage;
 
     @Bean(name = "message")
     @Lazy
     @Profile("test")
     public Message messageTest() {
-        return new MessageImpl("SimpleBeanInTest");
+        return new MessageImpl("SimpleBeanInTest: " + extraMessage);
     }
 
     @Bean(name = "message")
     @Lazy
     @Profile("dev")
     public Message message() {
-        return new MessageImpl("SimpleBean");
+        return new MessageImpl("SimpleBean: " + extraMessage);
     }
 
     //ConfigurationClassBeanDefinitionReader -> loadBeanDefinitionsForBeanMethod -> this.conditionEvaluator.shouldSkip
