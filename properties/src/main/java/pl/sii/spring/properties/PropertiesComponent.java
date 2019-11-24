87db980a4d3713eca1579c73c28f86c90287ee92
klimas7 <klimas7@gmail.com>
2019-11-24 16:18:46 +0100

    Properties #5: PropertySource order

--- properties/src/main/java/pl/sii/spring/properties/PropertiesComponent.java
+++ properties/src/main/java/pl/sii/spring/properties/PropertiesComponent.java
@@ -3,6 +3,7 @@ package pl.sii.spring.properties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.convert.Delimiter;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -10,7 +11,10 @@ import java.util.List;
 import java.util.logging.Logger;
 
 @Component
-@PropertySource("classpath:sii.properties")
+@PropertySources({
+        @PropertySource("classpath:sii2.properties"),
+        @PropertySource("classpath:sii.properties"),
+})
 public class PropertiesComponent {
     Logger logger = Logger.getLogger(this.getClass().getName());
 
@@ -35,6 +39,9 @@ public class PropertiesComponent {
     @Value("${sii.spring.p4}")
     private String p4;
 
+    @Value("${sii.spring.p4a}")
+    private String p4a;
+
     @Value("#{${sii.spring.p5} + 10}")
     private String p5;
 
@@ -50,6 +57,7 @@ public class PropertiesComponent {
         logger.info("sii.spring.p3a: " + p3a);
         logger.info("sii.spring.p3b: " + p3b);
         logger.info("sii.spring.p4: " + p4);
+        logger.info("sii.spring.p4a: " + p4a);
         logger.info("sii.spring.p5: " + p5);
         logger.info("userName: " + userName);
     }
