556aad132151e95ab4b092d8eaef0de61b55ddd6
klimas7 <klimas7@gmail.com>
2019-11-25 23:04:04 +0100

    diff #1: Diff between Autowired Inject Resource

---
+++ core/src/main/java/pl/sii/spring/core/diff/Triangle.java
@@ -0,0 +1,13 @@
+package pl.sii.spring.core.diff;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Triangle implements Shape {
+    int a = 5;
+
+    @Override
+    public int area() {
+        return (int) ((a * a * Math.sqrt(3)) / 2);
+    }
+}
