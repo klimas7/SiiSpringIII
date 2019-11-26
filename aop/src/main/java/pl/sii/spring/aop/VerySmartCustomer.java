3b248be585d2f21836e7f40eb54da0aa85f2cda2
klimas7 <klimas7@gmail.com>
2019-11-26 23:50:02 +0100

    AOP #7: AOP Around and change method parameter

--- aop/src/main/java/pl/sii/spring/aop/VerySmartCustomer.java
+++ aop/src/main/java/pl/sii/spring/aop/VerySmartCustomer.java
@@ -8,16 +8,20 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
+
+import static org.springframework.util.StringUtils.isEmpty;
 
 @Aspect
 @Component
 public class VerySmartCustomer {
-    public static final Log log = LogFactory.getLog(VerySmartCustomer.class);
-    Map<String, Integer> thingsCount = new HashMap<>();
+    private static final Log log = LogFactory.getLog(VerySmartCustomer.class);
+    private Map<String, Integer> thingsCount = new HashMap<>();
 
     @Pointcut("execution(* pl.sii.spring.aop.Shop.buy(..))")
     public void buy() {
@@ -27,7 +31,7 @@ public class VerySmartCustomer {
     public void addToBasket(String name) {
     }
 
-    @Before("addToBasket(name)")
+    @Before(value = "addToBasket(name)", argNames = "name")
     public void countThings(String name) {
         int count = getThingsCount(name);
         thingsCount.put(name, count + 1);
@@ -37,20 +41,41 @@ public class VerySmartCustomer {
         return Optional.ofNullable(thingsCount.get(name)).orElse(0);
     }
 
-    public void showCounts() {
+    void showCounts() {
         thingsCount.forEach((key, value) -> log.info(key + " " + value));
     }
 
-    @Around("buy()")
-    public void shopping(ProceedingJoinPoint pjp) {
-        log.info("Before shopping");
-
+    @Around(value = "addToBasket(name)", argNames = "pjp,name")
+    public Object aroundAddToBasket(ProceedingJoinPoint pjp, String name) {
+        if (isEmpty(name)) {
+            return null;
+        }
+        Object retValue = null;
         try {
-            pjp.proceed();
+            String newName = proceedName(name);
+            retValue = pjp.proceed(new String[]{newName});
         } catch (Throwable throwable) {
             log.error(throwable);
         }
 
+        return retValue;
+    }
+
+    private String proceedName(String name) {
+        return name.toUpperCase().trim();
+    }
+
+
+    @Around("buy()")
+    public Object shopping(ProceedingJoinPoint pjp) {
+        log.info("Before shopping");
+        Object retValue = null;
+        try {
+            retValue = pjp.proceed();
+        } catch (Throwable throwable) {
+            log.error(throwable);
+        }
         log.info("After shopping");
+        return retValue;
     }
 }
