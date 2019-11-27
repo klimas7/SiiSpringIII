7dd3503a48ea383fbe0fdb8909a8d3083eb0c4e8
klimas7 <klimas7@gmail.com>
2019-11-27 13:41:57 +0100

    SpringBoot #1 Skeleton application

---
+++ spring-boot/src/main/java/pl/sii/spring/boot/BootApplication.java
@@ -0,0 +1,12 @@
+package pl.sii.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class BootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class);
+    }
+}
