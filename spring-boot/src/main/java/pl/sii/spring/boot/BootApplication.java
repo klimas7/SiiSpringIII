e425abe9ac284136ac9cda626b553fb3a4313ae9
klimas7 <klimas7@gmail.com>
2019-11-27 13:42:22 +0100

    SpringBoot #2 Scheduled task

--- spring-boot/src/main/java/pl/sii/spring/boot/BootApplication.java
+++ spring-boot/src/main/java/pl/sii/spring/boot/BootApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BootApplication {
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class);
---
+++ spring-boot/src/main/java/pl/sii/spring/boot/ScheduledTask.java
@@ -0,0 +1,20 @@
+package pl.sii.spring.boot;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTask {
+    private static Log log = LogFactory.getLog(ScheduledTask.class);
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("The time is now: " + DATE_FORMAT.format(new Date()));
+    }
+}
