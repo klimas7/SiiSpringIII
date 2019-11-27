f49c4d486a7138f8bef3586ec9094bbb479d7103
klimas7 <klimas7@gmail.com>
2019-11-27 13:59:10 +0100

    SpringBoot #5 Simple endpoint and entity

--- spring-boot/src/main/java/pl/sii/spring/boot/ApiController.java
+++ spring-boot/src/main/java/pl/sii/spring/boot/ApiController.java
@@ -1,7 +1,10 @@
 package pl.sii.spring.boot;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +14,11 @@ public class ApiController {
     @GetMapping("/helloSii")
     public String helloSii() {
         return "Hello Sii";
+    }
+
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(@RequestParam String name) {
+        return new ResponseEntity<>("Hello " + name, HttpStatus.OK);
     }
 }
