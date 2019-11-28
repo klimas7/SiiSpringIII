7d3f0690687f67897a9e8936000f8b6b86303953
klimas7 <klimas7@gmail.com>
2019-11-28 22:44:56 +0100

    Jms #4 Simple example textPlain queue

---
+++ jms/src/main/java/pl/sii/spring/jms/JmsConfig.java
@@ -0,0 +1,7 @@
+package pl.sii.spring.jms;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JmsConfig {
+}
--- jms/src/main/java/pl/sii/spring/jms/MessageServiceImpl.java
+++ jms/src/main/java/pl/sii/spring/jms/MessageServiceImpl.java
@@ -1,16 +1,40 @@
 package pl.sii.spring.jms;
 
+import org.apache.activemq.command.ActiveMQTextMessage;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.jms.core.JmsOperations;
 import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
 
 @Component
 public class MessageServiceImpl implements MessageService {
+    private static final String QUEUE_PLAIN_TEXT = "message.queue.plainText";
+
+    private static final Log log = LogFactory.getLog(MessageServiceImpl.class);
+
+    private JmsOperations jmsOperations;
+
+    public MessageServiceImpl(JmsOperations jmsOperations) {
+        this.jmsOperations = jmsOperations;
+    }
+
     @Override
     public void sendMessage(String message) {
-
+        jmsOperations.send(QUEUE_PLAIN_TEXT, session -> session.createTextMessage(message));
     }
 
     @Override
     public String getMessage() {
+        Message receive = jmsOperations.receive(QUEUE_PLAIN_TEXT);
+        try {
+            return ((TextMessage)receive).getText();
+        } catch (JMSException e) {
+            log.error(e);
+        }
         return null;
     }
 
