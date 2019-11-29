6596b4acb6143002f50c18dc8401f3f85514cea1
klimas7 <klimas7@gmail.com>
2019-11-29 23:39:47 +0100

    DB #1: Empty application

---
+++ db/src/main/java/pl/sii/spring/db/DbApplication.java
@@ -0,0 +1,11 @@
+package pl.sii.spring.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DbApplication.class);
+    }
+}
