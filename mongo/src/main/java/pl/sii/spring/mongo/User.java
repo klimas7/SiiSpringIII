05267f68b1a8c415e217e4352bfe7ffb7320fcb8
klimas7 <klimas7@gmail.com>
2019-11-29 21:47:24 +0100

    Mongo #3: Add user model

---
+++ mongo/src/main/java/pl/sii/spring/mongo/User.java
@@ -0,0 +1,64 @@
+package pl.sii.spring.mongo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+
+@Document
+public class User {
+    @Id
+    private String id;
+    private String firstName;
+    private String secondName;
+    private Integer age;
+    private LocalDateTime birthDate;
+
+    public User(String firstName, String secondName, Integer age, LocalDateTime birthDate) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+        this.birthDate = birthDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+}
