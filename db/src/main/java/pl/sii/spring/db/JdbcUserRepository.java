91784d09eb771da8a44c8543665a3093b0a70b5e
klimas7 <klimas7@gmail.com>
2019-11-30 21:44:33 +0100

    DB #4: Implements count and save

--- db/src/main/java/pl/sii/spring/db/JdbcUserRepository.java
+++ db/src/main/java/pl/sii/spring/db/JdbcUserRepository.java
@@ -1,15 +1,23 @@
 package pl.sii.spring.db;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import pl.sii.spring.db.model.User;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository("jdbc")
 public class JdbcUserRepository implements UserRepository {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public long count() {
-        return 0;
+        return jdbcTemplate.queryForObject("SELECT count(*) FROM user", Long.class);
     }
 
     @Override
@@ -29,11 +37,24 @@ public class JdbcUserRepository implements UserRepository {
 
     @Override
     public User save(User user) {
-        return null;
+        long id = insertUserAndReturnId(user);
+        User newUser = new User(id, user.getFirstName(), user.getLastName(), user.getUserAge(), user.getBirthDate());
+        return newUser;
     }
 
     @Override
     public void delete(long id) {
 
+    }
+
+    private long insertUserAndReturnId(User user) {
+        SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate).withTableName("user");
+        insert.setGeneratedKeyName("id");
+        Map<String, Object> args= new HashMap();
+        args.put("firstName", user.getFirstName());
+        args.put("lastName", user.getLastName());
+        args.put("age", user.getUserAge());
+        args.put("birthDate", user.getBirthDate());
+        return insert.executeAndReturnKey(args).longValue();
     }
 }
